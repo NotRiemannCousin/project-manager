6da110310984b2711d869e57aef19ff69d47867f
MarceloH1122 <marcelohenrique8822@gmail.com>
2023-02-03 21:22:54 -0300

    fixed recusive memory bug. Passwords now can have numbers.

--- dev/create/validate.php
+++ dev/create/validate.php
@@ -28,14 +28,13 @@ if (
 
 R::setup('mysql:host=127.0.0.1;dbname=GerenciadorProjetos', 'root');
 
-$nome = preg_replace("/[^\p{L}\s]/u", "", $_POST['nome']);
+$nome = preg_replace("/[^\p{L}]/u", "", $_POST['nome']);
 $nasc = $_POST['nasc'];
 $nivel = $_POST['nivel'];
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$senha = preg_replace("/[^\p{L}]/u", "", $_POST['senha']);
+$senha = preg_replace("/[^\p{L}|\d|=|+|-|\|]/u", "", $_POST['senha']);
 $ativo = (isset($_POST['ativo']) && $_POST['ativo'] == 'on' ? 1 : 0);
 $admin = (isset($_POST['admin']) && $_POST['admin'] == 'on' ? 1 : 0);
-
 
 
 $dev  = R::dispense('desenvolvedor');
@@ -50,10 +49,8 @@ $cred->email         = $email;
 $cred->senha         = $senha;
 $cred->ativo         = $ativo;
 $cred->admin         = $admin;
-$cred->desenvolvedor = $dev;
 
 R::store($dev);
-R::store($cred);
 
 header('Location: ../list/');
 R::close();
